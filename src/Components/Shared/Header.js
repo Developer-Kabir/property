47700be82446fcf0078ee6b05292e800ec489be7
Developer Kabir <hakabir40@gmail.com>
2022-09-21 09:16:43 +0600

    header fixed

--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -1,46 +1,36 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Container, Nav, Navbar} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 
 
 
 const Header = () => {
     return (
-        <div>
-            <div class="site-mobile-menu site-navbar-target">
-		<div class="site-mobile-menu-header">
-			<div class="site-mobile-menu-close">
-				<span class="icofont-close js-menu-toggle"></span>
-			</div>
+        <div className='site-nav'>
+
+		<div className='container'>
+		<Navbar className='menu-bg-wrap' collapseOnSelect expand="lg">
+      <Container >
+        <Navbar.Brand as={Link} to="/" className='logo'>MyProperty</Navbar.Brand>
+        <Navbar.Toggle style={{color: "white"}} aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+        
+          <Nav className='ms-auto'>
+            <Nav.Link as={Link} to="/" className='kabir'>Home</Nav.Link>
+            <Nav.Link as={Link} to="/property" className='kabir'>Properties</Nav.Link>
+            <Nav.Link as={Link} to="/about" className='kabir'>About</Nav.Link>
+            <Nav.Link as={Link} to="/service" className='kabir'>Service</Nav.Link>
+            <Nav.Link as={Link} to="/contact" className='kabir'>Contacts</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
 		</div>
-		<div class="site-mobile-menu-body"></div>
-	</div>
-
-	<nav class="site-nav">
-		<div class="container">
-			<div class="menu-bg-wrap">
-				<div class="site-navigation">
-					<Link to="/" class="logo  float-start">MyProperty</Link>
-					<ul class="js-clone-nav d-none d-lg-inline-block text-start site-menu float-end">
-						<li><Link to="/">Home</Link></li>
-						<li>
-							<Link to="/property">Properties</Link>
-						</li>
-						<li><Link to="/service">Services</Link></li>
-						<li><Link to="/about">About</Link></li>
-						<li><Link to="/contact">Contact Us</Link></li>
-					</ul>
-
-					<Link to="#" class="burger light me-auto float-end mt-1 site-menu-toggle js-menu-toggle d-inline-block d-lg-none" data-toggle="collapse" data-target="#main-navbar">
-						<span></span>
-					</Link>
-
-				</div>
-			</div>
-		</div>
-	</nav>
-
 	
-        </div>
+</div>
+	
+        
     );
 };
 
